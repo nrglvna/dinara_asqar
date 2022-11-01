a3a7eab16bbe51c6eef68995a4b87ff494d8f951
nrglvna <askarkyzyng@gmail.com>
2022-11-01 19:45:47 +0600

    sdfcsdf commit

--- src/components/ShowMore/ShowMore.jsx
+++ src/components/ShowMore/ShowMore.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { BsPlusCircleFill } from "react-icons/bs";
-const ShowMore = ({taqyryp,text1,text2,text3,text4}) => {
+const ShowMore = ({taqyryp,text1,text2,text3,text4,text5,text6,text7,text8,text9,text10}) => {
 
     const [isShowMoreShown,setShowMoreShown]=useState(false)
     const togglebtn=()=>{
@@ -18,6 +18,12 @@ const ShowMore = ({taqyryp,text1,text2,text3,text4}) => {
                 <li>{text2}</li>
                 <li>{text3}</li>
                 <li>{text4}</li>
+                <li>{text5}</li>
+                <li>{text6}</li>
+                <li>{text7}</li>
+                <li>{text8}</li>
+                <li>{text9}</li>
+                <li>{text10}</li>
             </ul>
             : <h3> {taqyryp}</h3>}
         
