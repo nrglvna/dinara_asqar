41ef0b2144abde13146f6eeb894937f15c2d228c
nrglvna <askarkyzyng@gmail.com>
2022-11-03 00:48:16 +0600

    sdfcsdf commit

--- src/container/Cases/Cases.jsx
+++ src/container/Cases/Cases.jsx
@@ -3,43 +3,103 @@ import {AiFillInstagram} from 'react-icons/ai';
 import images from '../../constants/images';
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import "swiper/css";
 import "swiper/css/navigation";
 import './Cases.css';
+import { useEffect } from 'react';
 
-const Cases = () => (
+const Cases = () => {
+  useEffect(()=>{
+    AOS.init({duration:2000});
+  },[])
+
+
+  return(
   <div className='app_case app__bg section__padding' id='cases'> 
-    <h2 className='headtext__cormorant' >БІЗДІҢ КЕЙСТЕР</h2>
+    <h2 className='headtext__opensans' >БІЗДІҢ КЕЙСТЕР</h2>
     <Swiper navigation={true} modules={[Navigation]} className="mySwiper_cases">
-      <SwiperSlide >
-        <div className="app_case_container app_wrapper_bg ">
+      <SwiperSlide className='app_wrapper_bg' >
+        <div className="app_case_container  ">
           <div className="app_case_content">
-            <h2 >Vavilon</h2>
+            <h2 className='h__opensans'>Vavilon</h2>
             <a href="" className='p__opensans'>insta</a>
             
 
-            <div className="app_case_content_box">
-              <p className='p__opensans'>Ссылка бойынша клик</p>
-              <h3>1217</h3>
-              <AiFillInstagram/>
+            <div className="app_case_content_box" data-aos='fade-up'>
+              <p className='p__opensans'>Kлик саны</p>
+              <h3 className='h__opensans'>1217</h3>
+              <AiFillInstagram className='insta_icon' />
             </div>
-            <div className="app_case_content_box">
-              <p className='p__opensans'>Ссылка бойынша клик</p>
-              <h3>1217</h3>
-              <AiFillInstagram/>
+            <div className="app_case_content_box" data-aos='fade-up'>
+              <p className='p__opensans'>Клик бағасы</p>
+              <h3 className='h__opensans'>0,09 $</h3>
+              <AiFillInstagram className='insta_icon'/>
             </div>
-            <div className="app_case_content_box">
-              <p className='p__opensans'>Ссылка бойынша клик</p>
-              <h3>1217</h3>
-              <AiFillInstagram/>
+            <div className="app_case_content_box" data-aos='fade-up'>
+              <p className='p__opensans'>Сатылым</p>
+              <h3 className='h__opensans'>30 000 000</h3>
+              <AiFillInstagram className='insta_icon' />
             </div>
+          </div>
+          <img src={images.case01} alt="" />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide className='app_wrapper_bg'>
+        <div className="app_case_container  ">
+          <div className="app_case_content">
+            <h2 className='h__opensans'>Vavilon</h2>
+            <a href="" className='p__opensans'>insta</a>
+            
 
+            <div className="app_case_content_box" data-aos='fade-left' data-aos-delay="2500">
+              <p className='p__opensans'>Kлик саны</p>
+              <h3 className='h__opensans'>1217</h3>
+              <AiFillInstagram className='insta_icon' />
+            </div>
+            <div className="app_case_content_box" data-aos='fade-left' data-aos-delay="2500">
+              <p className='p__opensans'>Клик бағасы</p>
+              <h3 className='h__opensans'>0,09 $</h3>
+              <AiFillInstagram className='insta_icon'/>
+            </div>
+            <div className="app_case_content_box" data-aos='fade-left' data-aos-delay="2500">
+              <p className='p__opensans'>Сатылым</p>
+              <h3 className='h__opensans'>30 000 000</h3>
+              <AiFillInstagram className='insta_icon' />
+            </div>
+          </div>
+          <img src={images.case01} alt="" />
+        </div>
+      </SwiperSlide>
+      <SwiperSlide className='app_wrapper_bg' >
+        <div className="app_case_container  ">
+          <div className="app_case_content">
+            <h2 className='h__opensans'>Vavilon</h2>
+            <a href="" className='p__opensans'>insta</a>
+            
+
+            <div className="app_case_content_box" data-aos='fade-left' data-aos-delay="6500">
+              <p className='p__opensans'>Kлик саны</p>
+              <h3 className='h__opensans'>1217</h3>
+              <AiFillInstagram className='insta_icon' />
+            </div>
+            <div className="app_case_content_box" data-aos='fade-right'>
+              <p className='p__opensans'>Клик бағасы</p>
+              <h3 className='h__opensans'>0,09 $</h3>
+              <AiFillInstagram className='insta_icon'/>
+            </div>
+            <div className="app_case_content_box" data-aos='fade-right'>
+              <p className='p__opensans'>Сатылым</p>
+              <h3 className='h__opensans'>30 000 000</h3>
+              <AiFillInstagram className='insta_icon' />
+            </div>
           </div>
           <img src={images.case01} alt="" />
         </div>
       </SwiperSlide>
     </Swiper>
   </div>
-);
-
+)
+}
 export default Cases;
